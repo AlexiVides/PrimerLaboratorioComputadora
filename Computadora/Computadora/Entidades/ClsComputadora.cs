b8309af614d4b161b70660d8cc4fb264d911d7a7
Nestor2021 <87671739+Nestor2021@users.noreply.github.com>
2021-08-16 15:13:26 -0600

    ClsComputadora

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,5 +8,27 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
+        private int memoria_ram;
+        private String procesador;
+        private String dico_duro;
+        private String placa_madre;
+        private String disipador_calor;
+
+        private String monitor;
+        private String teclado;
+        private String mouse;
+        private String upc;
+        private String audio;
+
+        public int Memoria_ram { get => memoria_ram; set => memoria_ram = value; }
+        public string Procesador { get => procesador; set => procesador = value; }
+        public string Dico_duro { get => dico_duro; set => dico_duro = value; }
+        public string Placa_madre { get => placa_madre; set => placa_madre = value; }
+        public string Disipador_calor { get => disipador_calor; set => disipador_calor = value; }
+        public string Monitor { get => monitor; set => monitor = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
+        public string Upc { get => upc; set => upc = value; }
+        public string Audio { get => audio; set => audio = value; }
     }
 }
