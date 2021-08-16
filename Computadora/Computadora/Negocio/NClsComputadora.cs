f07a6b7249334d037e483c90c75a9a1704bb23c7
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-08-16 15:23:12 -0600

    Funciones

--- Computadora/Computadora/Negocio/NClsComputadora.cs
+++ Computadora/Computadora/Negocio/NClsComputadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Computadora.Negocio
 {
     class NClsComputadora
     {
+        public String Apagar(ClsComputadora computadora)
+        {
+            return "Apagando computadora";
+        }
+        public String Acción(ClsComputadora computadora)
+        {
+            return "Abriendo programa";
+        }
+        public String Web(ClsComputadora computadora)
+        {
+            return "Cargando pagina";
+        }
     }
 }
